31d074840e6bc1c462330df9c8a7000c42b70ff4
carlapreda17 <carla_preda@yahoo.com>
2024-06-24 18:07:02 +0300

    add admin

--- pages/admin/add-admin.js
+++ pages/admin/add-admin.js
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import { createAdmin } from '../../api/persons';
 import { toaster } from '../../lib';
 import { useRouter } from 'next/router';
+import PersonForm from "../../components/Forms/PersonForm";
 
 const Page = () => {
   const router = useRouter();
@@ -29,67 +30,7 @@ const Page = () => {
     <Layout title="Timebook" role="admin">
       <div className="card max-w-xl">
         <h1 className="card-title">Adaugă administrator</h1>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={(values) => handleSubmit(values)}
-        >
-          <Form className="flex flex-col space-y-4">
-            <div className="flex ">
-              <div className="w-1/2 mr-6">
-                <Fieldset name="last_name" label="Nume">
-                  <Field id="last_name" name="last_name" as={Input} autoComplete="off" />
-                </Fieldset>
-              </div>
-              <div className="w-1/2">
-                <Fieldset name="first_name" label="Prenume">
-                  <Field id="first_name" name="first_name" as={Input} autoComplete="off" />
-                </Fieldset>
-              </div>
-            </div>
-            <div className="flex tablet:grid">
-              <div className="w-1/2 tablet:mb-5 sm:mr-6 tablet:w-full">
-                <Birthday />
-              </div>
-              <div className="w-1/2 tablet:mb-5 tablet:w-full">
-                <Fieldset name="job" label="Pozitie in cadrul companiei">
-                  <Field id="job" name="job" as={Input} autoComplete="off" />
-                </Fieldset>
-              </div>
-            </div>
-           <div className="tablet:mb-5">
-             <Fieldset name="email" label="E-mail">
-               <Field id="email" name="email" as={Email} autoComplete="new-email" />
-             </Fieldset>
-           </div>
-            <div className="flex tablet:grid">
-              <div className="w-1/2 tablet:mb-5 sm:mr-6 tablet:w-full">
-                <Fieldset name="password" label="Parola">
-                  <div className="mb-1" />
-                  <Field id="password" name="password" as={Password} autoComplete="new-password" />
-                </Fieldset>
-              </div>
-              <div className="w-1/2 tablet:mb-5 tablet:w-full">
-                <Fieldset name="password_confirm" label="Confirma parola">
-                  <div className="mb-1" />
-                  <Field id="password_confirm" name="password_confirm" as={Password} />
-                </Fieldset>
-              </div>
-            </div>
-            <div className="flex flex-row justify-between pt-8 sm:flex-row gap-4 sm:justify-between mt-6">
-              <Link href="/admin/profile">
-                <a className="cancel">
-                  <i className="fas fa-arrow-left"></i>
-                  <span className="ml-2 ">Înapoi</span>
-                </a>
-              </Link>
-              <Submit className="confirm">
-                <i className="fas fa-plus"></i>
-                <span className="ml-2">Adaugă</span>
-              </Submit>
-            </div>
-          </Form>
-        </Formik>
+       <PersonForm initialValues={initialValues} onSubmit={handleSubmit}></PersonForm>
       </div>
     </Layout>
   );
