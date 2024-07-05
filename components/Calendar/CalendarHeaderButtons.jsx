a4840990210abf3b99236a7228bc3de7eeb534ac
carlapreda17 <carla_preda@yahoo.com>
2024-07-05 19:02:22 +0300

    refactoring calendarHeaderButtons

--- components/Calendar/CalendarHeaderButtons.jsx
+++ components/Calendar/CalendarHeaderButtons.jsx
@@ -9,16 +9,11 @@ const CalendarHeaderButtons = ({ role, refetch }) => {
   return (
     <div className="flex gap-2 mobile:mt-3">
       {role === 'admin' ? (
-        <>
-          <LogbookButton/>
-          <AddActivityButton onHideModal={refetch}/>
-        </>
+        <LogbookButton/>
       ) : (
-        <>
-          <UserTimesheetButton/>
-          <AddActivityButton onHideModal={refetch}/>
-        </>
+        <UserTimesheetButton/>
       )}
+      <AddActivityButton onHideModal={refetch}/>
     </div>
   );
 };
