08117073789e56ff8eca8de3dc2ee8287df2f566
carlapreda17 <carla_preda@yahoo.com>
2024-07-05 19:49:23 +0300

    small fix

--- components/Calendar/CalendarHeaderButtons.jsx
+++ components/Calendar/CalendarHeaderButtons.jsx
@@ -5,7 +5,6 @@ import { useProfile } from '../../hooks';
 
 const CalendarHeaderButtons = ({ role, refetch }) => {
 
-
   return (
     <div className="flex gap-2 mobile:mt-3">
       {role === 'admin' ? (
